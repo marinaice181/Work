085a6611ecd3594e8a0c3b2edb680bcff15b594a
Marina <anfina1@yandex.ru>
2023-10-23 19:41:57 +0300

    100% покрытие

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,8 @@
-from utils.arrs import get_, my_slice
 from utils import arrs
 import pytest
 
 
 def test_get():
-    assert get_([1, 2, 3], 1, "test") == 2
-    assert get_([], 0, "test") == "test"
     assert arrs.get([1, 2, 3, 4, 5], 2) == 3
     assert arrs.get([1, 2, 3, 4, 5], 10, default="Not found") == "Not found"
     assert arrs.get([1, 2, 3, 4, 5], 3) != 5
@@ -15,8 +12,6 @@ def test_get():
 
 
 def test_slice():
-    assert my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
     assert arrs.my_slice([1, 2, 3, 4, 5]) != [1, 2, 3, 4]
     assert arrs.my_slice([1, 2, 3, 4, 5], 2) == [3, 4, 5]
@@ -28,4 +23,3 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], start=-5, end=5) == [1, 2, 3, 4, 5]
     assert arrs.my_slice([]) == []
     assert arrs.my_slice([1, 2, 3, 4, 5]) != []
-
