6970499fc018ad354883ba1c8f673449cbd85e1e
Marina <anfina1@yandex.ru>
2023-10-23 07:56:20 +0300

    функции pytest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,10 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1,2,3,4,5], -3) == [3, 4, 5]
+    assert arrs.my_slice([1,2,3,4,5], 10) == []
+    assert arrs.my_slice([1,2,3,4,5]) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], -4, -1) == [2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4, 5], -10) == [1, 2, 3, 4, 5]
